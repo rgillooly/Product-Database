4a479687a6896fb55cdb4974fa08a09972bbc82a
rgillooly <rpgillooly@gmail.com>
2023-11-21 15:13:15 -0500

    First Commit

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,7 +1,56 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+const actions = ["Add a Category", "View Categories", "Add a Product", "View Products", "Add A Product Tag",
+ "View Product Tags", "Add a Tag", "View Tags"]
+
 router.use('/api', apiRoutes);
+
+function getAction () {
+  inquirer.prompt([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      name: 'action',
+      choices: 'actions'
+    },
+
+  ])
+  .then (function(response) {
+    if (response.action == "Add a Category")
+    {
+      categoryAdd()
+    }
+    else if (response.action == "View Categories")
+    {
+      categoryView()
+    }
+    else if (response.action == "Add a Product")
+    {
+      productAdd()
+    }
+    else if (response.action == "View Products")
+    {
+      productView()
+    }
+    else if (response.action == "Add a Product Tag")
+    {
+      addProductTag()
+    }
+    else if (response.action == "View Product Tags")
+    {
+      viewProductTag()
+    }
+    else if (response.action == "Add a Tag")
+    {
+      addTag()
+    }
+    else if (response.action == "View Tags")
+    {
+      viewTag()
+    }
+  })
+}
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>")
--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const parsing = require("./routes/index")
 // import sequelize connection
 
 const app = express();
@@ -9,6 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+app.get("/" ,(req, res) => {
+res.send('Welcome to the Product Database. Would You Like To Begin? (Y/N)')
+if (res == Y) {
+  getAction()
+} else {app.quit}
+})
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
